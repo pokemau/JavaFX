12e736ef32f839529e02182963ed4bdec22a1cc3
pokemau <rentillosa90@gmail.com>
2024-03-11 17:09:22 +0800

    css

--- src/main/java/com/example/mau_javafx/HelloApplication.java
+++ src/main/java/com/example/mau_javafx/HelloApplication.java
@@ -1,22 +1,8 @@
 package com.example.mau_javafx;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.paint.Color;
-import javafx.scene.text.Font;
-import javafx.scene.text.FontWeight;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -38,6 +24,7 @@ public class HelloApplication extends Application {
         stage.setScene(scene);
         stage.setTitle("TEST");
         scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("hello.css")).toExternalForm());
+        stage.setResizable(false);
         stage.show();
     }
 
